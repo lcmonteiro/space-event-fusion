a496a573e9fe8151d4704faf8d4d641a6287e84d
lcmonteiro <monteiro.lcm@gmail.com>
2020-10-02 00:23:43 +0100

    update fusion

--- include/fusion.hpp
+++ include/fusion.hpp
@@ -4,24 +4,30 @@
 
 namespace Fusion {
 
+/// Kernel 
 class Kernel {};
 
+/// Space 
+/// @param func 
 template <typename Kernel, typename Main>
 void Space(Main func) {
     func(std::make_shared<Kernel>());
 }
 
+/// Scope
+/// @param home 
+/// @param func 
 namespace {
     template <typename Type, typename Home>
-    struct Decorator : Type {
-        Decorator(Home h) {}
+    struct Wrapper : Type {
+        Wrapper(Home h) {}
         template <typename Call>
         void wait(Call func) {}
     };
 } // namespace
 template <typename Type, typename Home, typename Main>
 void Scope(Home home, Main func) {
-    func(std::make_shared<Decorator<Type, Home>>(home), home);
+    func(std::make_shared<Wrapper<Type, Home>>(home), home);
 }
 
 } // namespace Fusion
